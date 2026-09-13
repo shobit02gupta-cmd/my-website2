ecc3b7e029f7dcbdb56c60a38661002abd39d189
shobit Gupta <shobit02.gupta@gmail.com>
2026-09-14 00:45:54 +0530

    vijaya website final with footer fix

---
+++ fun.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//     return a* b;
+// }
+// console.log(add(2, 3)); 
+// console.log(add(3,6));
+
+// function say(){
+//     return"jai hind"
+// }
+// console.log(say());
+
+//global scope//
+let a = 10;
+function ter(){
+    console.log(a);
+}
+console.log(a);
